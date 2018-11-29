4fc31a3b19596eab296e54e25117d4892368b34b
LeyangYu1996 <43018204+LeyangYu1996@users.noreply.github.com>
2018-11-29 12:53:51 -0500

    Add files via upload

--- Mini_project_3/With_mongoDB/search.py
+++ Mini_project_3/With_mongoDB/search.py
@@ -18,15 +18,36 @@ db = client.Pics
 posts = db.posts
 
 # Define a function to find tags and print the twitter ID and the urls.
-def find_tags_in_Twitter_ID(Tags):
+def find_tags_in_Twitter_ID():
+    print("You chose to search a tag in database.")
+    Tags = input("Please input the Tags you are looking for:")
     formal_ID = ''
+    check = 0
     for post in posts.find({"tags": Tags}).sort('Twitter_ID'):
+        check = 1
         ID = post.get('Twitter_ID', [])
         if(ID != formal_ID):
             formal_ID = ID
             print("*********************************************")
             print(ID,' has follwing pictures including tag ',Tags)
         print(post.get('link', []),' searched by ',post.get('User_name', []))
+    print("*********************************************")
+    if(check == 0):
+        print("*********************************************")
+        print("Sorry, there is no picture corresponding with this tag.")
+        print("*********************************************")
+
+def statistic():
+    formal_ID = ''
+    print("*********************************************")
+    for post in posts.find().sort('Twitter_ID'):
+        ID = post.get('Twitter_ID', [])
+        if(ID != formal_ID):
+            formal_ID = ID
+            count = posts.count_documents({"Twitter_ID": ID})
+            print("There is ",count," pictures downloaded from ",ID)
+    print("*********************************************")
+    
 
 if __name__ == '__main__':
     # Calculate the number of vaild data in the database
@@ -37,9 +58,16 @@ if __name__ == '__main__':
     # If there is vaild data, go on and search inside
     if(i != 0 ):
         print("There is ",i," vaild pictures in the database.")
-        Tags = input("Please input the Tags you are looking for:")
-        find_tags_in_Twitter_ID(Tags)
+        Flag = input("Do you want to search for a tag(T) or you want to check the statistics(S)?")
+        if(Flag == 'T'):
+            find_tags_in_Twitter_ID()
+        else:
+            if(Flag == 'S'):
+                statistic()
+            else:
+                print("Please input `T` to search for a tag, or `S` to check the statistics. Please check your input and try again.")
 
     # If there is no vaild data, skip searching and report and error
     else:
         print("ERROR: There is no vaild data in the database, please try executing the main.py and try again.")
+    print("Execute ends")
